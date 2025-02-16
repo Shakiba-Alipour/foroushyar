a80381e367df87054cf93107a2f3293a9fa35d51
Shakiba Alipour <alipour.sh79@gmail.com>
2025-02-16 11:52:54 +0330

    the dot under the active page's icon

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import AI from "../assets/AI_Icon";
 import HomeIcon from "../assets/Home_Icon";
 import FaqIcon from "../assets/Faq_Icon";
@@ -15,14 +15,29 @@ const Menu = () => {
     { path: "/", key: "", icon: <AI /> },
   ];
 
+  // current path
+  const location = useLocation();
+
   return (
     // display menu
     <div className="w-full h-14 bottom-0">
       <div className="flex justify-around items-center w-11/12 place-self-center h-12 bg-NeutralPrimaryBackground border-NeutralSecondaryBackgroundDark">
         {items.map(({ path, key, icon }) => (
-          <Link to={path} key={key}>
-            {icon}
-          </Link>
+          <div key={key} className="flex flex-col relative items-center">
+            <NavLink
+              to={path}
+              className={({ isActive }) =>
+                isActive ? "text-PrimaryDefault" : ""
+              }
+              end
+            >
+              {icon}
+            </NavLink>
+            {/* the red dot is only appeared when the page is active */}
+            {location.pathname === path && (
+              <span className="absolute bg-red-800 w-1 h-1 rounded-full bottom-[-10px]"></span>
+            )}
+          </div>
         ))}
       </div>
     </div>
