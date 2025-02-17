32ab25c29a38d52a9829395aac36670c483d0279
Shakiba Alipour <alipour.sh79@gmail.com>
2025-02-17 17:03:58 +0330

    routes are added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import Layout from "./layout/Layout";
+import FaqPage from "./pages/faq/FaqPage";
+import ProductsPage from "./pages/products/ProductsPage";
 
 // define routes
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/faq", element: <FaqPage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
   },
 ]);
 
