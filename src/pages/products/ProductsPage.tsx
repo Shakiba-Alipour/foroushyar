416cfc6e0a3e9249ea55067df0ace7edc4792e22
Shakiba Alipour <alipour.sh79@gmail.com>
2025-02-21 19:03:57 +0330

    searchArea is implemented globally

--- src/pages/products/ProductsPage.tsx
+++ src/pages/products/ProductsPage.tsx
@@ -1,27 +1,12 @@
-import { GetProps, Input } from "antd";
-import { Key } from "react";
 import GlobalButtons from "./components/GlobalButtons";
 import AllProducts from "./components/AllProducts";
+import SearchArea from "../../components/SearchArea";
 
 const ProductsPage = () => {
-  const { Search } = Input;
-
-  type SearchProps = GetProps<typeof Input.Search>;
-
-  // to handle search
-  const handleSearchClick: SearchProps["onSearch"] = (value, _e, info) =>
-    console.log(info?.source, value);
-
   return (
     <div className="flex-grow flex-col m-4">
       <h1 className="text-Text+Icon-01 font-bold">آگهی‌ها</h1>
-      <Search
-        placeholder="جستجوی آگهی"
-        allowClear
-        size="large"
-        onSearch={handleSearchClick}
-        className="p-4"
-      />
+      <SearchArea />
 
       <AllProducts />
 
