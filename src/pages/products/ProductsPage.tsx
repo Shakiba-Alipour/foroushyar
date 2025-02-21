0f3e3f2040dd369aa010d1dd5bb5b2d77f8c0d44
Shakiba Alipour <alipour.sh79@gmail.com>
2025-02-21 17:35:01 +0330

    products are moved to a seperate component

--- src/pages/products/ProductsPage.tsx
+++ src/pages/products/ProductsPage.tsx
@@ -1,8 +1,7 @@
-import { Button, GetProps, Input } from "antd";
-import { useLoaderData } from "react-router-dom";
-import AI_Icon from "../../assets/AI_Icon";
+import { GetProps, Input } from "antd";
 import { Key } from "react";
 import GlobalButtons from "./components/GlobalButtons";
+import AllProducts from "./components/AllProducts";
 
 const ProductsPage = () => {
   const { Search } = Input;
@@ -24,7 +23,7 @@ const ProductsPage = () => {
         className="p-4"
       />
 
-      <DisplayProducts />
+      <AllProducts />
 
       <GlobalButtons
         addonTitle="محصولات مرتبط همه"
@@ -33,42 +32,5 @@ const ProductsPage = () => {
     </div>
   );
 };
-
-// to display the products
-const DisplayProducts = () => {
-  // const products=useLoaderData()
-  const products = [
-    { id: 1, name: "عنوان محصول", image: "" },
-    { id: 2, name: "عنوان محصول", image: "" },
-  ];
-  return (
-    <div className="mt-2 mb-2 mr-6 ml-6">
-      {products.map((product: { id: number; name: string; image: string }) => (
-        <div className="mb-8 border-black" key={product.id}>
-          <div className="flex flex-col">
-            <p>{product.name}</p>
-            <div className="flex flex-row w-fit mt-4 mb-2">
-              <Button className="ml-1 font-semibold text-Primary-Default border-Primary-Background">
-                محصول مرتبط
-              </Button>
-              <Button
-                variant="solid"
-                className="mr-1 font-semibold bg-Success-Default text-Neutral-White stroke-Neutral-White"
-                icon={<AI_Icon />}
-                iconPosition="end"
-              >
-                ربات
-              </Button>
-            </div>
-          </div>
-          <img src={product.image} />
-        </div>
-      ))}
-    </div>
-  );
-};
-
-// to load the products
-export async function loader() {}
 
 export default ProductsPage;
---
+++ src/pages/products/components/AllProducts.tsx
@@ -0,0 +1,43 @@
+import { Button } from "antd";
+import AI_Icon from "../../../assets/AI_Icon";
+
+const AllProducts = () => {
+  // const products=useLoaderData()
+  const products = [
+    { id: 1, name: "عنوان محصول", image: "" },
+    { id: 2, name: "عنوان محصول", image: "" },
+  ];
+  return (
+    <div className="mt-2 mb-2 mr-4 ml-4">
+      {products.map((product: { id: number; name: string; image: string }) => (
+        <div
+          className="mb-8 border border-Text+bg-Text+Icon-05 rounded-rounded-10 p-3 w-full"
+          key={product.id}
+        >
+          <div className="flex flex-col">
+            <p>{product.name}</p>
+            <div className="flex flex-row w-fit mt-4 mb-2">
+              <Button className="ml-1 font-semibold text-Primary-Default border-Primary-Background">
+                محصول مرتبط
+              </Button>
+              <Button
+                variant="solid"
+                className="mr-1 font-semibold bg-Success-Default text-Neutral-White stroke-Neutral-White"
+                icon={<AI_Icon />}
+                iconPosition="end"
+              >
+                ربات
+              </Button>
+            </div>
+          </div>
+          <img src={product.image} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+// to load the products
+export async function loader() {}
+
+export default AllProducts;
