4d0ad952a0dc4c26903fdf64c1d79e357acbc0c4
Shakiba Alipour <alipour.sh79@gmail.com>
2025-03-01 16:10:05 +0330

    confirmation modal is added. styling is not complete yet

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -1,5 +1,4 @@
 import { Modal } from "antd";
-import { useState } from "react";
 import CancelSaveButton from "./CancelSaveButton";
 
 // to handle click on close or cancel buttons
@@ -10,48 +9,54 @@ const ConfirmationModal = ({
   isVisible,
   setIsVisible,
   onConfirm,
-  onCancel,
 }: {
   title: string;
   confirmLabel: string;
   cancelLabel: string;
   isVisible: boolean;
   setIsVisible: (isVisible: boolean) => void;
-  onConfirm: () => void;
-  onCancel: () => void;
+  onConfirm?: () => void;
 }) => {
+  // styling purposes
+  // const modalStyles = {
+  //   container: {
+  //     padding: 0,
+  //   },
+  //   footer: {
+  //     borderTop: "1px solid #e1e6ef",
+  //     backgroundColor: "#f8f9fc",
+  //     width: "100%",
+  //   },
+  // };
+
   return (
     <Modal
-      title={title}
       closable={false} // to remove default close button
-      // footer={false} // to remove default footer buttons
       open={isVisible}
-      className="bg-Neutral-White"
-      cancelText={cancelLabel}
-      okText={confirmLabel}
+      className="bg-Neutral-White rounded-rounded-6"
       centered
-      confirmLoading //apply loading visual effect for confirm button
-      destroyOnClose //unmount child components on onClose
-      width={90}
+      // set responsive width
+      width={{
+        xs: "90%",
+        sm: "80%",
+        md: "70%",
+        lg: "60%",
+        xl: "50%",
+        xxl: "40%",
+      }}
       zIndex={50}
-      onCancel={onCancel}
-      onOk={onConfirm}
-      cancelButtonProps={{
-        className:
-          "w-1/2 ml-2 font-bold bg-Neutral-White border-Danger-Background text-Danger-Default hover:text-Danger-Hover hover:border-Danger-Hover cursor-pointer",
-      }}
-      okButtonProps={{
-        className:
-          "w-1/2 mr-2 font-bold text-Neutral-White bg-Primary-Default hover:bg-Primary-Hover cursor-pointer",
-      }}
+      // styles={modalStyles}
+      //props of CancelSaveButton are not correct
+      footer={
+        <CancelSaveButton
+          whiteButtonLabel={cancelLabel}
+          redButtonLabel={confirmLabel}
+          onWhiteClick={() => setIsVisible(false)}
+          onRedClick={() => setIsVisible(false)}
+        />
+      }
     >
-      {/* props of CancelSaveButton are not correct */}
-      {/* <CancelSaveButton
-        whiteButtonLabel={cancelLabel}
-        redButtonLabel={confirmLabel}
-        onWhiteClick={() => setIsVisible(true)}
-        onRedClick={() => setIsVisible(true)}
-      /> */}
+      <p className="font-bold m-4 bg-Neutral-White">{title}</p>
     </Modal>
   );
 };
--- src/pages/faq/components/DisplayLoadedFaq.tsx
+++ src/pages/faq/components/DisplayLoadedFaq.tsx
@@ -3,6 +3,8 @@ import Edit_Icon from "../../../assets/Edit_Icon";
 import { useLoaderData } from "react-router-dom";
 import Delete_Icon from "../../../assets/Delete_Icon";
 import NewQuestion from "./NewQuestion";
+import ConfirmationModal from "../../../components/ConfirmationModal";
+import { useState } from "react";
 
 const dataSource = [
   { key: 1, question: "چگونه می‌توانم محصول را در دیوار خریداری کنم؟" },
@@ -16,6 +18,9 @@ const DisplayLoadedFaq = ({
   isDrawerOpen: boolean;
   setIsDrawerOpen: (isDrawerOpen: boolean) => void;
 }) => {
+  const [isConfirmationModalVisible, setIsConfirmationModalVisible] =
+    useState(false);
+
   // the columns configuration
   const columns = [
     {
@@ -29,16 +34,8 @@ const DisplayLoadedFaq = ({
       key: "actions",
       render: () => (
         <div className="flex flex-row justify-around cursor-pointer">
-          <Edit_Icon
-            onClick={() => {
-              // setIsDrawerOpen(true);
-              <NewQuestion
-                isDrawerOpen={isDrawerOpen}
-                setIsDrawerOpen={setIsDrawerOpen}
-              />;
-            }}
-          />
-          <Delete_Icon />
+          <Edit_Icon onClick={() => setIsDrawerOpen(true)} />
+          <Delete_Icon onClick={() => setIsConfirmationModalVisible(true)} />
         </div>
       ),
       width: 90,
@@ -49,23 +46,42 @@ const DisplayLoadedFaq = ({
   const data = useLoaderData();
 
   // if no questions had been entered before
-  if (!Array.isArray(data) || data.length === 0) {
-    return (
-      <p className="text-Neutral-LineDark flex justify-center m-4 align-middle">
-        هیچ سوالی وجود ندارد
-      </p>
-    );
-  }
+  // if (!Array.isArray(data) || data.length === 0) {
+  //   return (
+  //     <p className="text-Neutral-LineDark flex justify-center m-4 align-middle">
+  //       هیچ سوالی وجود ندارد
+  //     </p>
+  //   );
+  // }
 
   //   if a list of questions is available
   return (
-    <Table
-      dataSource={dataSource}
-      columns={columns}
-      pagination={false}
-      bordered
-      className="w-11/12 flex justify-self-center"
-    />
+    <>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={false}
+        bordered
+        className="w-11/12 flex justify-self-center"
+      />
+
+      {isDrawerOpen && (
+        <NewQuestion
+          isDrawerOpen={isDrawerOpen}
+          setIsDrawerOpen={setIsDrawerOpen}
+        />
+      )}
+
+      {isConfirmationModalVisible && (
+        <ConfirmationModal
+          title="آیا از حذف مطمئن هستید؟"
+          confirmLabel="حذف"
+          cancelLabel="انصراف"
+          isVisible={isConfirmationModalVisible}
+          setIsVisible={setIsConfirmationModalVisible}
+        />
+      )}
+    </>
   );
 };
 
