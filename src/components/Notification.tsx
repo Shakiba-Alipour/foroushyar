bac8804f7373ac5156ae9a90d47a795f2bb78458
Shakiba Alipour <alipour.sh79@gmail.com>
2025-03-04 11:30:59 +0330

    notification component is added to cover error handling and display success and failure messages

---
+++ src/components/Notification.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, message, Space } from "antd";
+
+// This component contains the structure of every notification in the app (error, success, etc)
+
+interface NotificationProps {
+  text: string;
+  isSuccessful: boolean; //true for success, false for error
+}
+
+const Notification: React.FC<NotificationProps> = ({ text, isSuccessful }) => {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const display = () => {
+    messageApi.open({
+      type: isSuccessful ? "success" : "error",
+      content: text,
+    });
+  };
+
+  return (
+    <>
+      {contextHolder}
+      {display}
+    </>
+  );
+};
+
+export default Notification;
