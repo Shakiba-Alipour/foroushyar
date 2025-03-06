1c845545939dfa3fba2768f78bdd2984fc0c0863
Shakiba Alipour <alipour.sh79@gmail.com>
2025-03-06 16:56:49 +0330

    Created typed versions of the useDispatch and useSelector hooks to not to import the RootState and AppDispatch types into each component

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
@@ -7,7 +8,15 @@ export const store = configureStore({
   },
 });
 
+export default store;
+// Get the type of the store variable
+export type AppStore = typeof store;
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+type RootState = ReturnType<AppStore["getState"]>;
+type AppDispatch = AppStore["dispatch"];
+
+// Created typed versions of the useDispatch and useSelector hooks to not
+// to import the RootState and AppDispatch types into each component
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
